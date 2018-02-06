6b83bf6447d190c25b27bedb4f05f336c34db5de
silvioprog <silvioprog@gmail.com>
2018-02-06 13:50:09 -0300

    Changes all "Brook4*" occurrences to "Brook library".

--- cmake/WarnAll.cmake
+++ cmake/WarnAll.cmake
@@ -3,26 +3,26 @@
 #   | |_) | | |_) |  | | | | | | | | | |/ /
 #   |  _ <  |  _ <   | | | | | | | | |   (
 #   | |_) | | | \ \  | |_| | | |_| | | |\ \
-#   |_____/ |_|  \_\ \_____/ \_____/ |_| \_\ 4-REST.
+#   |_____/ |_|  \_\ \_____/ \_____/ |_| \_\
 #
 #   –– a small library which helps you write quickly REST APIs.
 #
-# Copyright (c) 2012-2018 Silvio Clecio, et al.
+# Copyright (c) 2012-2018 Silvio Clecio.
 #
-# This file is part of Brook4-REST.
+# This file is part of Brook library.
 #
-# Brook4-REST is free software: you can redistribute it and/or modify
+# Brook library is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# Brook4-REST is distributed in the hope that it will be useful,
+# Brook library is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Lesser General Public License for more details.
 #
 # You should have received a copy of the GNU Lesser General Public License
-# along with Brook4-REST.  If not, see <http://www.gnu.org/licenses/>.
+# along with Brook library.  If not, see <http://www.gnu.org/licenses/>.
 #
 
 option(ENABLE_PICKY_COMPILER "Enable picky compiler options" ON)
