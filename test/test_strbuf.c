9c6b150a0804ec4779db4f588d342d16182e1cd0
silvioprog <silvioprog@gmail.com>
2018-02-12 16:16:41 -0300

    [tests/string-buffer] adds initial testing.

---
+++ test/test_strbuf.c
@@ -0,0 +1,43 @@
+/*    _____   _____    _____   _____   _   __
+ *   |  _  \ |  _  \  /  _  \ /  _  \ | | / /
+ *   | |_) | | |_) |  | | | | | | | | | |/ /
+ *   |  _ <  |  _ <   | | | | | | | | |   (
+ *   | |_) | | | \ \  | |_| | | |_| | | |\ \
+ *   |_____/ |_|  \_\ \_____/ \_____/ |_| \_\
+ *
+ *   –– a small library which helps you write quickly REST APIs.
+ *
+ * Copyright (c) 2012-2018 Silvio Clecio <silvioprog@gmail.com>
+ *
+ * This file is part of Brook library.
+ *
+ * Brook library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Brook library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Brook library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "bk_assert.h"
+
+#include "brook.h"
+
+int main(void) {
+    struct bk_strbuf *sb;
+
+    /* Checks if a string-buffer object was successfully created. */
+    sb = bk_strbuf_new();
+    ASSERT(sb);
+    ASSERT(sizeof(sb) == sizeof(struct bk_strbuf *));
+
+    /* There is no a portable way to test if a memory has been freed, so just free it. */
+    bk_strbuf_free(sb);
+    return EXIT_SUCCESS;
+}
