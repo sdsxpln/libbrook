a5d4944a770d5248a454b07a2a768269e57f77e4
silvioprog <silvioprog@gmail.com>
2018-02-18 19:51:33 -0300

    [doc/api] change all occurrences of "@" to "\" following the Doxygens examples.

--- include/brook.h
+++ include/brook.h
@@ -26,11 +26,11 @@
  */
 
 /**
- * @mainpage
- * @li @ref bk_api
+ * \mainpage
+ * \li \ref bk_api
  */
 
-/** @defgroup bk_api API reference
+/** \defgroup bk_api API reference
  * The API reference grouped by feature.
  * */
 
@@ -50,45 +50,45 @@ extern "C" {
 #define BK_VERSION_HEX ((BK_VERSION_MAJOR << 16) | (BK_VERSION_MINOR <<  8) | (BK_VERSION_PATCH))
 
 /**
- * @ingroup bk_api
- * @defgroup bk_utils Utilities
+ * \ingroup bk_api
+ * \defgroup bk_utils Utilities
  * All utility functions of the library.
- * @{
+ * \{
  */
 
 /**
  * Returns the library version number.
- * @return Library version packed into a single integer.
+ * \return Library version packed into a single integer.
  */
 extern unsigned int bk_version(void);
 
 /**
  * Returns the library version number as string.
- * @return Library version packed into a static string.
+ * \return Library version packed into a static string.
  */
 extern const char *bk_version_str(void);
 
 /**
  * Allocates a new memory space and zero-initialize it.
- * @param[in] size Memory size to be allocated.
- * @return Pointer of the allocated zero-initialized memory.
- * @retval NULL When size is `0` or no memory space.
+ * \param[in] size Memory size to be allocated.
+ * \return Pointer of the allocated zero-initialized memory.
+ * \retval NULL When size is `0` or no memory space.
  */
 extern void *bk_alloc(size_t size);
 
 /**
  * Frees a memory space previous allocated by #bk_alloc().
- * @param[in] ptr Pointer of the memory to be freed.
+ * \param[in] ptr Pointer of the memory to be freed.
  */
 extern void bk_free(void *ptr);
 
-/** @} */
+/** \} */
 
 /**
- * @ingroup bk_api
- * @defgroup bk_str String
+ * \ingroup bk_api
+ * \defgroup bk_str String
  * String structure and its related functions.
- * @{
+ * \{
  */
 
 /**
@@ -98,88 +98,88 @@ struct bk_str;
 
 /**
  * Creates a new string instance zero-initialized.
- * @return String instance zero-initialized.
- * @retval NULL When no memory space.
+ * \return String instance zero-initialized.
+ * \retval NULL When no memory space.
  */
 extern struct bk_str *bk_str_new(void);
 
 /**
  * Frees a string instance previous allocated by #bk_str_new().
- * @param[in] str Pointer of the string instance to be freed.
+ * \param[in] str Pointer of the string instance to be freed.
  */
 extern void bk_str_free(struct bk_str *str);
 
 /**
- * Writes a zero-terminated value to string handle @p str. All values previously written are kept.
- * @param[in] str String handle.
- * @param[in] val Value to be written.
- * @param[in] len Length of the value to be written including the terminating null byte (<tt>`\0`</tt>).
- * @retval 0 - Success.
- * @retval -EINVAL - Invalid argument.
+ * Writes a zero-terminated value to string handle \p str. All values previously written are kept.
+ * \param[in] str String handle.
+ * \param[in] val Value to be written.
+ * \param[in] len Length of the value to be written including the terminating null byte (<tt>`\0`</tt>).
+ * \retval 0 - Success.
+ * \retval -EINVAL - Invalid argument.
  */
 extern int bk_str_write(struct bk_str *str, const char *val, size_t len);
 
 /**
- * Reads a zero-terminated value from string handle @p str.
- * @param[in] str String handle.
- * @param[out] val Value to be read.
- * @param[in,out] len Pointer to specify then store the value length including the terminating null byte (<tt>`\0`</tt>).
- * @retval 0 - Success.
- * @retval -EINVAL - Invalid argument.
- * @retval -ENOBUFS - No buffer space available.
+ * Reads a zero-terminated value from string handle \p str.
+ * \param[in] str String handle.
+ * \param[out] val Value to be read.
+ * \param[in,out] len Pointer to specify then store the value length including the terminating null byte (<tt>`\0`</tt>).
+ * \retval 0 - Success.
+ * \retval -EINVAL - Invalid argument.
+ * \retval -ENOBUFS - No buffer space available.
  */
 extern int bk_str_read(struct bk_str *str, char *val, size_t *len);
 
 /**
- * Writes a formatted value from variable argument list to string handle @p str.
- * @param[in] str String handle.
- * @param[in] fmt Formatted string (following the same [`printf()`](https://linux.die.net/man/3/printf) format
+ * Writes a formatted value from variable argument list to string handle \p str.
+ * \param[in] str String handle.
+ * \param[in] fmt Formatted string (following the same [`printf()`](https://linux.die.net/man/3/printf) format
  *  specification).
- * @param[in] ap Arguments list variable (handled by
+ * \param[in] ap Arguments list variable (handled by
  *  [`va_start()`](https://linux.die.net/man/3/va_start)/[`va_end()`](https://linux.die.net/man/3/va_end)).
- * @retval 0 - Success.
- * @retval -EINVAL - Invalid argument.
+ * \retval 0 - Success.
+ * \retval -EINVAL - Invalid argument.
  */
 extern int bk_str_printf_va(struct bk_str *str, const char *fmt, va_list ap);
 
 /**
- * Writes a formatted value to string handle @p str.
- * @param[in] str String handle.
- * @param[in] fmt Formatted string (following the same [`printf()`](https://linux.die.net/man/3/printf) format
+ * Writes a formatted value to string handle \p str.
+ * \param[in] str String handle.
+ * \param[in] fmt Formatted string (following the same [`printf()`](https://linux.die.net/man/3/printf) format
  *  specification).
- * @param ... Additional arguments (following the same [`printf()`](https://linux.die.net/man/3/printf) arguments
+ * \param ... Additional arguments (following the same [`printf()`](https://linux.die.net/man/3/printf) arguments
  *  specification).
- * @retval 0 - Success.
- * @retval -EINVAL - Invalid argument.
+ * \retval 0 - Success.
+ * \retval -EINVAL - Invalid argument.
  */
 extern int bk_str_printf(struct bk_str *str, const char *fmt, ...);
 
 /**
- * Gets the zero-terminated string content from handle @p str.
- * @param[in] str String handle.
- * @return Content as static string.
- * @retval NULL When Invalid argument.
+ * Gets the zero-terminated string content from handle \p str.
+ * \param[in] str String handle.
+ * \return Content as static string.
+ * \retval NULL When Invalid argument.
  */
 extern const char *bk_str_content(struct bk_str *str);
 
 /**
- * Gets the total string length written in the handle @p str.
- * @param[in] str String handle.
- * @param[out] len Pointer to store the string length.
- * @retval 0 - Success.
- * @retval -EINVAL - Invalid argument.
+ * Gets the total string length written in the handle \p str.
+ * \param[in] str String handle.
+ * \param[out] len Pointer to store the string length.
+ * \retval 0 - Success.
+ * \retval -EINVAL - Invalid argument.
  */
 extern int bk_str_length(struct bk_str *str, size_t *len);
 
 /**
- * Cleans all written string present in the handle @p str.
- * @param[in] str String handle.
- * @retval 0 - Success.
- * @retval -EINVAL - Invalid argument.
+ * Cleans all written string present in the handle \p str.
+ * \param[in] str String handle.
+ * \retval 0 - Success.
+ * \retval -EINVAL - Invalid argument.
  */
 extern int bk_str_clear(struct bk_str *str);
 
-/** @} */
+/** \} */
 
 #ifdef __cplusplus
 }
