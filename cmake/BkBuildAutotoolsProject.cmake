1719b5f534b161a82d6219984f760ea1e954b2cd
silvioprog <silvioprog@gmail.com>
2018-03-22 21:39:15 -0300

    [cmake/build-autotools-pkg] adds new module that builds a autotools project.

---
+++ cmake/BkBuildAutotoolsProject.cmake
@@ -0,0 +1,61 @@
+#TODO: this module is experimental, it will be documented as soon as it was done
+
+if (__BK_BUILD_AUTOTOOLS_PROJECT_INCLUDED)
+    return()
+endif ()
+set(__BK_BUILD_AUTOTOOLS_PROJECT_INCLUDED ON)
+
+include(CMakeParseArguments)
+
+function(build_autotools_project)
+    set(_args DIR OPTIONS)
+    set(_options QUIET)
+    cmake_parse_arguments("" "${_options}" "${_args}" "" ${ARGN})
+    unset(_options)
+    unset(_args)
+    if ("${_DIR}" STREQUAL "")
+        message(FATAL_ERROR "DIR should not be empty.")
+    endif ()
+    set(_configure "../configure")
+    if (_OPTIONS)
+        set(_configure "${_configure} ${_OPTIONS}")
+    endif ()
+    set(_build_dir "${_DIR}/build")
+    file(REMOVE_RECURSE ${_build_dir})
+    file(MAKE_DIRECTORY ${_build_dir})
+    if (NOT _QUIET)
+        message(STATUS "Configuring ${_NAME}...")
+    endif ()
+    execute_process(COMMAND ${_configure}
+            WORKING_DIRECTORY ${_build_dir}
+            RESULT_VARIABLE _result
+            OUTPUT_QUIET
+            ERROR_QUIET)
+    if (NOT _result EQUAL 0)
+        file(REMOVE_RECURSE ${_DIR})
+        message(FATAL_ERROR "Error configuring ${_NAME}")
+    endif ()
+    unset(_result)
+    unset(_configure)
+    if (NOT _QUIET)
+        message(STATUS "Configuring ${_NAME}... done")
+    endif ()
+    if (NOT _QUIET)
+        message(STATUS "Building ${_NAME}...")
+    endif ()
+    execute_process(COMMAND ${CMAKE_MAKE_PROGRAM}
+            WORKING_DIRECTORY ${_build_dir}
+            RESULT_VARIABLE _result
+            OUTPUT_QUIET
+            ERROR_QUIET)
+    if (NOT _result EQUAL 0)
+        message(FATAL_ERROR "Error building ${_NAME}")
+    endif ()
+    unset(_result)
+    if (NOT _QUIET)
+        message(STATUS "Building ${_NAME}... done")
+    endif ()
+    set(${_NAME}_BUILD_DIR "${_build_dir}" PARENT_SCOPE)
+    unset(_build_dir)
+    unset(_DIR)
+endfunction()
