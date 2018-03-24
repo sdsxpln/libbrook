0d86ae91a9d2f6afabe916ef2233aa9cb04384da
silvioprog <silvioprog@gmail.com>
2018-03-24 02:24:09 -0300

    [*] removes cfg structure because we will store all configurations inside the server one.

--- src/bk_httpsrv.c
+++ src/bk_httpsrv.c
@@ -1,23 +1,12 @@
 #include "brook.h"
-#include "bk_httpsrv_cfg.h"
 #include "bk_httpsrv.h"
 
 struct bk_httpsrv *bk_httpsrv_new(void) {
     struct bk_httpsrv *srv = bk_alloc(sizeof(struct bk_httpsrv));
-    if (!srv)
-        return NULL;
-    srv->cfg = bk__bk_httpsrv_cfg_new();
-    if (!srv->cfg) {
-        bk_httpsrv_free(srv);
-        return NULL;
-    }
     return srv;
 }
 
 void bk_httpsrv_free(struct bk_httpsrv *srv) {
-    if (!srv)
-        return;
-    bk__bk_httpsrv_cfg_free(srv->cfg);
     bk_free(srv);
 }
 
--- src/bk_httpsrv.h
+++ src/bk_httpsrv.h
@@ -1,10 +1,8 @@
 #ifndef BK_HTTPSRV_H
 #define BK_HTTPSRV_H
 
-#include "bk_httpsrv_cfg.h"
-
 struct bk_httpsrv {
-    struct bk_httpsrv_cfg *cfg;
+    char *upload_dir;
 };
 
 #endif /* BK_HTTPSRV_H */
